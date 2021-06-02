84abbf4deee593e18729d0201a413ed232ebbe48
Valdecir Sarat <valdecir.sarat@gmail.com>
2021-06-02 00:33:31 -0300

    Primeira versão do Front-End

---
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import Stock from 'src/app/shared/models/stock-model';
+import { DashboardService } from '../dashboard.service';
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrls: ['./dashboard-page.component.css']
+})
+export class DashboardPageComponent implements OnInit {
+
+ stocks: Stock[] = [];
+
+  constructor(private dashboradService: DashboardService) { }
+
+  ngOnInit(): void {
+    this.fetchStocks();
+   }
+
+  async fetchStocks(): Promise<void> {
+    this.stocks = await this.dashboradService.getStocks();
+    console.log(this.stocks);
+
+  }
+
+}
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+﻿class Stock{
+  id: number;
+  name: string;;
+  price: number;
+  minimum: number;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+}
+
+export default Stock;
